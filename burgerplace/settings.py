bd9e663b05bd6b6d6905717100a66158109dfe6f
collinskoech11 <collinskoechck34@gmail.com>
2021-12-19 15:37:08 +0300

    djongo database engne

--- burgerplace/settings.py
+++ burgerplace/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'burgerplace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'burgerplacedb',
     }
 }
 
